f6d0421cc810a19f872d7b3cc6a5ba1a1a540270
Darshanbs2001 <darshandarsha59@gmail.com>
2025-02-01 19:59:26 +0530

    added the simple test for the createUser method

--- backend/movieVoting/src/test/java/com/movievoting/movieVoting/services/UserServiceImpTest.java
+++ backend/movieVoting/src/test/java/com/movievoting/movieVoting/services/UserServiceImpTest.java
@@ -1,25 +1,22 @@
 package com.movievoting.movieVoting.services;
 
 import com.movievoting.movieVoting.dto.UserDto;
-import com.movievoting.movieVoting.entities.User;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.modelmapper.ModelMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class UserServiceImpTest {
     @Autowired
     UserService service;
 
-   @Test
-    public void testcreateUser(){
-       UserDto userToSave=new UserDto("vinay b s","Vi@9141525275","vinay@gmail.com");
-       assertNotNull(service.createUser(userToSave));
-   }
+    @Test
+    public void testcreateUser() {
+        UserDto userToSave = new UserDto("vinay b s", "Vi@9141525275", "vinay@gmail.com");
+        assertNotNull(service.createUser(userToSave));
+    }
 }
