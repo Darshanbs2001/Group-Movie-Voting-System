c81f7a974d23659365bf4790333b4b7d69d9b982
Darshanbs2001 <darshandarsha59@gmail.com>
2025-02-01 19:59:56 +0530

    added the simple test for the createUser method

--- backend/movieVoting/src/main/java/com/movievoting/movieVoting/dto/UserDto.java
+++ backend/movieVoting/src/main/java/com/movievoting/movieVoting/dto/UserDto.java
@@ -4,9 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
 
