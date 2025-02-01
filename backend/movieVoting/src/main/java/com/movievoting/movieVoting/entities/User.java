609fecd52e342c2eb723ac2630d6976367471f4d
Darshanbs2001 <darshandarsha59@gmail.com>
2025-02-01 21:31:05 +0530

    added the h2 database and also made the support for the application.properties in the testing environment

--- backend/movieVoting/src/main/java/com/movievoting/movieVoting/entities/User.java
+++ backend/movieVoting/src/main/java/com/movievoting/movieVoting/entities/User.java
@@ -16,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @AllArgsConstructor
 @ToString
 @Entity
+@Table(name="app_users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
